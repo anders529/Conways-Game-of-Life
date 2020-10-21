23cc67ceee28bb2b218b71de5f757f331658c676
Aaron Andersen <aaronandersen@zohomail.com>
2020-10-20 21:27:49 -0500

    MVP almost met.

--- src/components/gol.js
+++ src/components/gol.js
@@ -1,63 +1,92 @@
 import React, { useState, useCallback, useRef } from "react";
 import produce from "immer";
+import './gol.css';
 
-const numRows = 25;
-const numCols = 25;
-const operations = [
-    [0, 1],
-    [0, -1],
-    [1, -1],
-    [-1, 1],
-    [1, 1],
-    [-1, -1],
-    [1, 0],
-    [-1, 0]
-];
+const numRows = 25
+const numCols = 25
+const operations = 
+[
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+]
 
 const generateEmptyGrid = () => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++) {
-        rows.push(Array.from(Array(numCols), () => 0));
-    }
-    return rows;
-};
+  const rows = []
 
-const Gol: React.FC = () => {
-    const [grid, setGrid] = useState(() => {
-        return generateEmptyGrid();
-    });
-    const [running, setRunning] = useState(false);
-    const runningRef = useRef(running);
-    runningRef.current = running;
-    const runSimulation = useCallback(() => {
-        if (!runningRef.current) {
-            return;
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0))
+  }
+  return rows
+}
+
+export default function Gol() {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid()
+  })
+
+  const [sum, setSum] = useState(0)
+  const [genZ, setGenZ] = useState(0)
+  const timmyRef = useRef(300)
+  const [running, setRunning] = useState(false)
+  const runningRef = useRef(running)
+  runningRef.current = running
+
+  const runSim = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+    setGrid((g) => {
+      let isGrid = false
+      isGrid = false
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let neighbors = 0
+            operations.forEach(([x, y]) => {
+              const newI = i + x
+              const newJ = j + y
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += g[newI][newJ]
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              isGrid = true
+              gridCopy[i][j] = 1
+            }
+          }
         }
-        setGrid(g => {
-            return produce(g, gridCopy => {
-                for (let i = 0; i < numRows; i++) {
-                    for (let k = 0; k < numCols; k++) {
-                        let neighbors = 0;
-                        operations.forEach(([x, y]) => {
-                            const newI = i + x;
-                            const newK = k + y;
-                            if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
-                                neighbors += g[newI][newK];
-                            }
-                        });
-                        if (neighbors < 2 || neighbors > 3) {
-                            gridCopy[i][k] = 0;
-                        } else if (g[i][k] === 0 && neighbors === 3) {
-                            gridCopy[i][k] = 1;
-                        }
-                    }
-                }
-            });
-        });
-        setTimeout(runSimulation, 100);
-    }, []);
+
+        if (isGrid) {
+          setGenZ((num) => num + 1)
+        }
+
+        setSum(
+          gridCopy.flat().reduce((gpp, pp) => {
+            return gpp + pp
+        })
+        )
+      })
+    })
+    setTimeout(runSim, timmyRef.current)
+  }, [])
+
     return (
         <div>
+            <div className='info-container'>
+                <div className='data'>
+                    <p>Generation: {genZ}</p>
+                    <p>Population: {sum}</p>
+                </div>
+            </div>
             <div
                 style={{
                     display: "grid",
@@ -77,20 +106,20 @@ const Gol: React.FC = () => {
                             style={{
                                 width: 20,
                                 height: 20,
-                                backgroundColor: grid[i][k] ? "pink" : undefined,
+                                backgroundColor: grid[i][k] ? "black" : undefined,
                                 border: "solid 1px black"
                             }}
                         />
                     ))
                 )}
             </div>
-            <div>
+            <div className={"buttons"}>
                 <button
                     onClick={() => {
                         setRunning(!running);
                         if (!running) {
                             runningRef.current = true;
-                            runSimulation();
+                            runSim();
                         }
                     }}
                 >
@@ -116,9 +145,28 @@ const Gol: React.FC = () => {
                 >
                     clear
                 </button>
+                <button
+            onClick={() => {
+              timmyRef.current = 5
+            }}
+          >
+            Speed Up
+          </button>
+          <button
+            onClick={() => {
+              timmyRef.current = 300
+            }}
+          >
+            Normal
+          </button>
+          <button
+            onClick={() => {
+              timmyRef.current = 800
+            }}
+          >
+            Slow Down
+          </button>
             </div>
         </div>
-    );
-};
-
-export default Gol;
+    )
+}
