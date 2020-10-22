8e69b1b292af1f3fb2cfd104584969d31b09bc54
Aaron Andersen <aaronandersen@zohomail.com>
2020-10-21 20:51:36 -0500

    Clear Button now clears out the generation!

--- src/components/gol.js
+++ src/components/gol.js
@@ -139,6 +139,7 @@ export default function Gol() {
                 <button
                     onClick={() => {
                         setGrid(generateEmptyGrid());
+                        setGenZ(0);
                     }}>
                     clear
                 </button>
